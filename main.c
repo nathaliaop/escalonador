8627dcad1ffd9cd11d6c29cd8a84763fe3546993
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-06-23 11:43:27 -0300

    Fixes identation

--- main.c
+++ main.c
@@ -11,105 +11,103 @@ const long ONE_SECOND = 6e8;
 // 3 - demorado
 
 int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    // -work-stealing
+    printf("You passed the flag: %s\n", argv[1]);
+  }
 
-    if (argc > 1) {
-      // -work-stealing
-      printf("You passed the flag: %s\n", argv[1]);
-    }
+  // 0 - leitura
+  // 1 - escrita
+  int fd[4][2];
+  int auxiliar_process_id = 0;
 
-
-    // 0 - leitura
-    // 1 - escrita
-    int fd[4][2];
-    int auxiliar_process_id = 0;
-
-    for (int i = 0; i < 4; i++) {
-        if (pipe(fd[i]) == -1) {
-            printf("Could not create pipe\n");
-            return 1;
-        };
-    }
+  for (int i = 0; i < 4; i++) {
+    if (pipe(fd[i]) == -1) {
+      printf("Could not create pipe\n");
+      return 1;
+    };
+  }
     
-    for (int i = 0; i < 4; i++) {
-        int id = fork();
-        if (id == 0) {
-            for (int j = 0; j < 4; j++) {
-                close(fd[j][1]);
-            }
+  for (int i = 0; i < 4; i++) {
+    int id = fork();
+    if (id == 0) {
+      for (int j = 0; j < 4; j++) {
+        close(fd[j][1]);
+      }
             
-            int n_processes;
-            read(fd[auxiliar_process_id][0], &n_processes, sizeof(int));
-            // printf("Quantity of processes: %d\n", n_processes);
+      int n_processes;
+      read(fd[auxiliar_process_id][0], &n_processes, sizeof(int));
+      // printf("Quantity of processes: %d\n", n_processes);
 
-            char process_type;
-            for (int j = 0; j < n_processes; j++) {
-                read(fd[auxiliar_process_id][0], &process_type, sizeof(char));
+      char process_type;
+      for (int j = 0; j < n_processes; j++) {
+        read(fd[auxiliar_process_id][0], &process_type, sizeof(char));
 
-                int seconds = 0;
+        int seconds = 0;
 
-                if (process_type == '1') seconds = 5;
-                else if (process_type == '2') seconds = 10;
-                else if (process_type == '3') seconds = 20;
+        if (process_type == '1') seconds = 5;
+        else if (process_type == '2') seconds = 10;
+        else if (process_type == '3') seconds = 20;
 
-                for (long j = 0; j < seconds * ONE_SECOND; j++) {} // simula o processo (prefiro com fork())
+        for (long j = 0; j < seconds * ONE_SECOND; j++) {} // simula o processo (prefiro com fork())
 
-                printf("Esperei %d segundos\n", seconds);
-            }
+        printf("Esperei %d segundos\n", seconds);
+      }
 
-            for (int j = 0; j < 4; j++) {
-                close(fd[j][0]);
-            }
+      for (int j = 0; j < 4; j++) {
+        close(fd[j][0]);
+      }
 
-            printf("%d finalizou seus processos.\n", auxiliar_process_id);
+      printf("%d finalizou seus processos.\n", auxiliar_process_id);
 
-            return 0;
-        }
-
-        auxiliar_process_id++;
+      return 0;
     }
 
-    for (int i = 0; i < 4; i++) {
-        close(fd[i][0]);
-    }
+    auxiliar_process_id++;
+  }
 
-    int file = open("processes.txt", O_RDONLY);
-    if (file == -1) {
-        printf("Could not open file\n");
-        return 2;
-    }
+  for (int i = 0; i < 4; i++) {
+    close(fd[i][0]);
+  }
 
-    char process_type;
-    int curr_id = 0;
+  int file = open("processes.txt", O_RDONLY);
+  if (file == -1) {
+    printf("Could not open file\n");
+    return 2;
+  }
 
-    char processes_type[4][512];
-    int nxt[4] = {};
+  char process_type;
+  int curr_id = 0;
 
-    while(read(file, &process_type, 1) != 0) {
-        if (process_type == '\n') continue;
+  char processes_type[4][512];
+  int nxt[4] = {};
 
-        processes_type[curr_id][nxt[curr_id]] = process_type;
-        nxt[curr_id]++;
-        curr_id = (curr_id + 1) % 4;
-    }
+  while(read(file, &process_type, 1) != 0) {
+    if (process_type == '\n') continue;
 
-    for (int i = 0; i < 4; i++) {
-        write(fd[i][1], &nxt[i], sizeof(int));
+    processes_type[curr_id][nxt[curr_id]] = process_type;
+    nxt[curr_id]++;
+    curr_id = (curr_id + 1) % 4;
+  }
+
+  for (int i = 0; i < 4; i++) {
+    write(fd[i][1], &nxt[i], sizeof(int));
         
-        for (int j = 0; j < nxt[i]; j++) {
-            write(fd[i][1], &processes_type[i][j], sizeof(char));
-        }
+    for (int j = 0; j < nxt[i]; j++) {
+      write(fd[i][1], &processes_type[i][j], sizeof(char));
     }
+  }
 
-    for (int i = 0; i < 4; i++) {
-        close(fd[i][1]);
-    }
+  for (int i = 0; i < 4; i++) {
+    close(fd[i][1]);
+  }
 
-    close(file);
+  close(file);
 
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
+  wait(NULL);
+  wait(NULL);
+  wait(NULL);
+  wait(NULL);
 
-    return 0;
+  return 0;
 }
